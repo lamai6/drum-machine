2474c8e1bf43f671afec3ce3b1b14c03577507ab
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-07 20:06:44 +0200

    test: fix us#3

--- src/components/DrumMachine/DrumMachine.test.jsx
+++ src/components/DrumMachine/DrumMachine.test.jsx
@@ -22,10 +22,14 @@ describe('DrumMachine test suite', () => {
 
   it('should render 9 clickable drum pad elements, each with .drum-pad, a unique id, and an inner text corresponding to his key (US #3)', () => {
     const { container } = render(<DrumMachine />);
-    const outerContainer = container.querySelectorAll('div[class=drum-pad]');
+    const drumPads = container.querySelectorAll('div[class=drum-pad]');
 
-    expect(outerContainer).toHaveLength(9);
-    expect(outerContainer[0].id).toBe('Heater-1');
-    expect(outerContainer[0].childNodes[0].innerHTML).toBe('Q');
+    const key = Array.from(drumPads[0].childNodes).filter(
+      (elem) => elem.tagName === 'SPAN'
+    )[0].innerHTML;
+
+    expect(drumPads).toHaveLength(9);
+    expect(drumPads[0].id).toBe('Heater-1');
+    expect(key).toBe('Q');
   });
 });
