eced2f402466c797a2f95a874658297d8e9a7e1d
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-08 03:11:48 +0200

    feat(us#5): add sound play on drumpad click

--- src/components/DrumPad/DrumPad.jsx
+++ src/components/DrumPad/DrumPad.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
 import './DrumPad.styles.scss';
 
@@ -6,13 +6,33 @@ class DrumPad extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.audioRef = createRef();
+    this.playSound = this.playSound.bind(this);
+  }
+
+  playSound() {
+    const { current: audio } = this.audioRef;
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
   }
 
   render() {
     const { soundId, soundSrc, keyTrigger } = this.props;
     return (
-      <div className="drum-pad" id={soundId}>
-        <audio className="clip" id={keyTrigger} src={soundSrc}>
+      <div
+        onClick={this.playSound}
+        className="drum-pad"
+        id={soundId}
+        role="button"
+        tabIndex={0}
+      >
+        <audio
+          ref={this.audioRef}
+          className="clip"
+          id={keyTrigger}
+          src={soundSrc}
+        >
           <track kind="captions" label={soundId} />
         </audio>
         <span>{keyTrigger}</span>
