6f28750ca934d18aa619b3bc03fabec35a7b4c7a
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-12 14:40:33 +0200

    feat(fcc): set right project for fcc test suite

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,8 @@ import DrumMachine from '../DrumMachine/DrumMachine';
 import './App.styles.scss';
 
 function App() {
+  localStorage.setItem('project_selector', 'drum-machine');
+
   return (
     <div id="container">
       <DrumMachine />
