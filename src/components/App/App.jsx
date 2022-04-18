368923925d150240fff5ba515e5f943fb90a7880
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-18 18:06:02 +0200

    feat(fcc): hide fcc test suite on page load

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import './App.styles.scss';
 
 function App() {
   localStorage.setItem('project_selector', 'drum-machine');
+  localStorage.setItem('fCC_drum-machine_hide', 'true');
 
   return (
     <div id="container">
